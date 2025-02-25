6a5736e26e566fe41ebd8def8e1d29bd4b6fc105
ruprekhab <ruprekhab@gmail.com>
2025-02-24 21:24:29 -0500

    Files added

---
+++ ERD.sql
@@ -0,0 +1,20 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/3zgMBG
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "loan" (
+    "age" numeric   NOT NULL,
+    "income" numeric   NOT NULL,
+    "home_ownership" Varchar(10)   NOT NULL,
+    "employment_duration" numeric   NOT NULL,
+    "loan_purpose" Varchar(30)   NOT NULL,
+    "loan_grade" Varchar(3)   NOT NULL,
+    "loan_amount" numeric   NOT NULL,
+    "int_rate" numeric   NOT NULL,
+    "loan_status" numeric   NOT NULL,
+    "loan_income_pct" float   NOT NULL,
+    "past_default_status" varchar(2)   NOT NULL,
+    "credit_history_length" numeric   NOT NULL
+);
+
